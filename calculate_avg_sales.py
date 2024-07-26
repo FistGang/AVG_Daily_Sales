171ea406fb5b8b4631b2e89ef1f402bdaf9ceba6
FistGang <20520671@gm.uit.edu.vn>
2024-07-26 08:57:43 +0700

    chore: update script

--- calculate_avg_sales.py
+++ calculate_avg_sales.py
@@ -1,5 +1,5 @@
 from pyspark.sql import SparkSession
-from pyspark.sql.functions import avg
+from pyspark.sql.functions import avg, round
 
 spark = (
     SparkSession.builder.master("local")
@@ -9,13 +9,15 @@ spark = (
 
 file_path = "./daily_sales.csv"
 data = spark.read.csv(file_path, header=True, inferSchema=True)
-average_sale = data.groupBy("store_id").agg(
-    avg("sales_amount").alias("average_daily_sales")
+average_sale = (
+    data.groupBy("store_id")
+    .agg(round(avg("sales_amount"), 2).alias("average_daily_sales"))
+    .orderBy("store_id")
 )
 
 average_sale.show()
 
 output_path = "average_daily_sales.csv"
-average_sale.write.csv(output_path, header=True)
+average_sale.write.mode("overwrite").csv(output_path, header=True)
 
 spark.stop()
--- generate_sample_data.py
+++ generate_sample_data.py
@@ -3,10 +3,10 @@ import csv
 from datetime import datetime, timedelta
 
 file_path = "daily_sales.csv"
-store_ids = [f"store{i}" for i in range(1, 101)]
+store_ids = [i for i in range(1, 101)]
 start_date = datetime(2023, 1, 1)
 end_date = datetime(2023, 12, 31)
-delta = end_date = start_date
+delta = end_date - start_date
 
 
 with open(file_path, mode="w", newline="") as file:
@@ -15,7 +15,7 @@ with open(file_path, mode="w", newline="") as file:
     for i in range(10000):
         store_id = random.choice(store_ids)
         sales_amount = round(random.uniform(100, 5000), 2)
-        date = start_date + timedelta(days=random.randint(0, delta.day))
+        date = start_date + timedelta(days=random.randint(0, delta.days))
         writer.writerow([store_id, sales_amount, date.strftime("%Y-%m-%d")])
 
 print(f"Sample sales data file created as {file_path}")
